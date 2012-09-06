81f3c91d792f336432dc1bbbbb9ccd208d4a561d
guan yuxin <guanyuxin128@gmail.com>
2012-09-06 16:14:16 +0800

    修改bug

--- docs/api.js
+++ docs/api.js
@@ -2,8 +2,11 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "Array",
+        "Browser",
         "Class",
+        "Dispatcher",
         "Function",
+        "KolaElement",
         "Object",
         "Packager",
         "String",
@@ -11,6 +14,9 @@ YUI.add("yuidoc-meta", function(Y) {
     ],
     "modules": [
         "kola",
+        "kola.bom",
+        "kola.event",
+        "kola.html",
         "kola.lang"
     ],
     "allModules": [
@@ -18,6 +24,20 @@ YUI.add("yuidoc-meta", function(Y) {
             "displayName": "kola",
             "name": "kola",
             "description": "kola是一个面向于大型、富交互Web应用的基础框架"
+        },
+        {
+            "displayName": "kola.bom",
+            "name": "kola.bom"
+        },
+        {
+            "displayName": "kola.event",
+            "name": "kola.event",
+            "description": "kola.event.Dispatcher 包, 事件分发者"
+        },
+        {
+            "displayName": "kola.html",
+            "name": "kola.html",
+            "description": "kola html Element 包，提供dom元素的封装33"
         },
         {
             "displayName": "kola.lang",
